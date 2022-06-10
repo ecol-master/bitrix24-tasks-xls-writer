b2766c3adf12b188db83657f5be7ca7b22d74473
EcolMaster <cloudym444@gmail.com>
2022-06-10 10:18:14 +0500

    main

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-from tools import DriverManager
-import config
+from tools import DriverManager, ExelManager
+import config, os
 
 def main():
-    driver_manager = DriverManager(user_login=config.USER_LOGIN, user_password=config.USER_PASSWORD)
-    # , bitrix_name="b24-xx803c.bitrix24.ru"
+    
+    # create managers driver & exel
+    driver_manager = DriverManager(user_login=config.USER_LOGIN, user_password=config.USER_PASSWORD, 
+                    bitrix_name=config.USER_BITRIX_NAME)
+    exel_manager = ExelManager(dir_json=config.DIR_JSON, dir_exel=config.DIR_EXEL)
+    
     try:
+        # authtarizate in bitrix24
         driver_manager.auth_bitrix24()
 
+        # parse usertasks from profile in bitrix24
         driver_manager.parse_bitrix24_usertasks()
+
+        # write to json file usertasks info
+        driver_manager.write_to_file_time_table_blocks_data()
+        
+        # close driver manager
+        driver_manager.finish()
+        
+        # write data to exel file from json file
+        exel_manager.write_exel_data()
+
     except Exception as error:
         print(f"{error=}")
-    finally:
-        driver_manager.finish()
 
 if __name__ == "__main__":
     main()
